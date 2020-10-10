8470aa6ff374fc7e839fa34db5d3b38112600bf2
Ashraf <deva4rf@gmail.com>
2020-10-10 03:15:43 -0400

    Fixed react-scripts

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 
 const AppStyles = styled.div.attrs({
-  className: "w-full h-screen flex flex-col items-center justify-center"
+  className: "w-full h-screen flex flex-col items-center justify-center",
 })`
   & {
     h1 {
-      ${tw`font-sans text-6xl font-hairline text-6xl text-teal-500`}
+      ${tw` text-6xl  font-bold text-6xl text-teal-500`}
     }
     p {
       ${tw`text-gray-700 text-lg`}
